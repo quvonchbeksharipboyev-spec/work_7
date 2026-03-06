ca4602198f64ad81b0e1b4be8e78f41c44014e62
quvonchbeksharipboyev-spec <quvonchbeksharipboyev@gmail.com>
2026-03-06 09:21:33 +0300

    Add files via upload

---
+++ work_7.py
@@ -0,0 +1,78 @@
+#if1
+# a=int(input("a="))
+# if a>0:
+#    print(a+1)
+# else:
+#    print(a)
+#if2
+# a=int(input("a="))
+# if a>0:
+#     print(a+1)
+# else:
+#     print(a-2)
+#if3
+# a=int(input("a="))
+# if a>0:
+#     print(a+1)
+# elif a<0:
+#     print(a-2)
+# else:
+#     print("a=10")
+#if4
+# a,b,c=map(int, input().split())
+# n=0
+# if a>0 and b<0 and c<0:
+#     print(n+1)
+# elif a<0 and b>0 and c<0:
+#     print(n+1)
+# elif a<0 and b<0 and c>0:
+#     print(n+1)
+# elif a>0 and b>0 and c<0:
+#     print(n+2)
+# elif a>0 and b<0 and c>0:
+#     print(n+2)
+# elif a<0 and b>0 and c>0:
+#     print(n+2)
+# elif a>0 and b>0 and c>0:
+#     print(n+3)
+# else:
+#     print(n)
+#if5
+# a,b,c=map(int, input().split())
+# musbat=0
+# manfiy=0
+# if (a>0 and b<0 and c<0) or (a<0 and b>0 and c<0) or (a<0 and b<0 and c>0):
+#     print(musbat+1, manfiy+2)
+# elif (a>0 and b>0 and c<0) or (a<0 and b>0 and c>0) or (a>0 and b<0 and c>0):
+#     print(musbat+2, manfiy+1)
+# elif (a>0 and b>0 and c>0):
+#     print(musbat+3)
+# else:
+#     print(manfiy+3)
+#if6
+# a=int(input("a="))
+# b=int(input("b="))
+# if a>b:
+#     print("Barno mani sevadi")
+# elif a<b:
+#     print("Barnovoy I love you")
+# else:
+#     print("do'g'ri bar sonni maymun")
+#if7
+# a=int(input("a="))
+# b=int(input("b="))
+# if a>b:
+#     print(b)
+# elif a<b:
+#     print(a)
+# else:
+#     print("maymuncham")
+#if8
+# a=int(input("a="))
+# b=int(input("b="))
+# if a>b:
+#     print(a, b)
+# elif a<b:
+#     print(b, a)
+# else:
+#     print("hech narsa")
